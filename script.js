e86fb09eb376577b48a64f87d78d80c14bef6cc4
Kshitij adhikaree <adhikareekhsitij@gmail.com>
2023-02-28 20:01:40 +0545

    fetch joke api

--- script.js
+++ script.js
@@ -107,15 +107,36 @@ const VoiceRSS = {
   },
 };
 
-function test() {
-  VoiceRSS.speech({
-    key: "159701a3cc8d4e49b5de3b365bc09479",
-    src: "Hello, world!",
-    hl: "en-us",
-    v: "Linda",
-    r: 0,
-    c: "mp3",
-    f: "44khz_16bit_stereo",
-    ssml: false,
-  });
+// function test() {
+//   VoiceRSS.speech({
+//     key: "159701a3cc8d4e49b5de3b365bc09479",
+//     src: "Hello, world!",
+//     hl: "en-us",
+//     v: "Linda",
+//     r: 0,
+//     c: "mp3",
+//     f: "44khz_16bit_stereo",
+//     ssml: false,
+//   });
+// }
+
+// Get jokes from joke api
+
+async function getJokes() {
+  let joke = "";
+  const apiUrl = "https://v2.jokeapi.dev/joke/Any";
+  try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    if (data.setup) {
+      joke = `${data.setup} ... ${data.delivery}`;
+    } else {
+      joke = data.joke;
+    }
+    console.log(joke);
+  } catch (error) {
+    //error here
+    console.log(error);
+  }
 }
+getJokes();
